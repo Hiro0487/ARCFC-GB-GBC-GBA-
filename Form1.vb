341595be7ef067cc085de2680cebdfa184fddb6f
Hiroyuki0487 <Hiroyuki0487@users.noreply.github.com>
2024-10-10 14:50:46 -0400

    Dev (#7)
    * Fix proper startupform to load
    
    * Updated File handeling
    
    * Disabled mGBA to mBoy! for the time being still trying to figgure that one out

--- Form1.vb
+++ Form1.vb
@@ -3,90 +3,109 @@ Imports System.Text
 Imports System.Xml
 
 Public Class Form1
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    ' Browse button click event
+    ' Displays file dialog and updates input/output file paths
     Private Sub BrowseButton_Click(sender As Object, e As EventArgs) Handles BrowseButton.Click
-        Dim inputFileDialog As New OpenFileDialog With {
-            .Title = "Select Cheat File",
-            .Filter = "Cheat Files (*.CHT)|*.CHT"
-        }
-        If inputFileDialog.ShowDialog = DialogResult.OK Then
-            inputFileTextBox.Text = inputFileDialog.FileName
-            OutputFileLabel.Text = Path.ChangeExtension(inputFileDialog.FileName, ".cheats")
+        Dim inputFile As String = ShowFileDialog("Select Cheat File", "Cheat Files (*.CHT)|*.CHT")
+        If Not String.IsNullOrEmpty(inputFile) Then
+            inputFileTextBox.Text = inputFile
+            OutputFileLabel.Text = Path.ChangeExtension(inputFile, ".cheats")
         End If
     End Sub
 
-    ' Extract button click event
+    ' Handles the extraction process
     Private Sub ExtractButton_Click(sender As Object, e As EventArgs) Handles ExtractButton.Click
-        ProgressBar.Value = 0 ' Set progress bar to 0
-
+        ProgressBar.Value = 0
         Dim inputFile As String = inputFileTextBox.Text
-        Dim outputFile As String
+        Dim outputFile As String = ShowFileDialog("Save Extracted Code", "Cheat Files (*.cheats)|*.cheats", False)
 
-        Dim saveFileDialog As New SaveFileDialog With {
-            .Title = "Save Extracted Code",
-            .Filter = "Cheat Files (*.cheats)|*.cheats"
-        }
-        If saveFileDialog.ShowDialog = DialogResult.OK Then
-            outputFile = saveFileDialog.FileName
-        Else
-            MsgBox("Please choose a file to save the extracted code.", vbInformation, "Save Location Required")
+        If String.IsNullOrEmpty(outputFile) Then
+            ShowMessage("Please choose a file to save the extracted code.", "Save Location Required")
             Exit Sub
         End If
 
         Try
-            Dim xmlDoc As New XmlDocument
-            xmlDoc.Load(inputFile)
-            Dim outputString As New StringBuilder
-            Dim cheatCount = xmlDoc.SelectNodes("//cheat").Count
-
-            For i As Integer = 1 To cheatCount
-                Dim cheatElement As XmlElement = xmlDoc.SelectNodes("//cheat")(i - 1)
-                Dim name As String = cheatElement.GetAttribute("name")
-                Dim codeBlock As New StringWriter
-
-                ProgressBar.Value = (i / cheatCount) * 100
-
-                For Each childNode In cheatElement.ChildNodes
-                    If childNode.NodeType = XmlNodeType.Element Then
-                        codeBlock.Write(childNode.InnerText & Environment.NewLine)
-                    ElseIf childNode.NodeType = XmlNodeType.Text Then
-                        Dim textContent As String = childNode.InnerText
-                        codeBlock.Write(Mid(textContent, 2, textContent.Length - 2) & Environment.NewLine) ' Extract excluding tags
-                    End If
-                Next
-
-                Dim codeString As String = codeBlock.ToString()
-
-                outputString.AppendLine("!disabled")
-                outputString.AppendLine("#" & name)
-                outputString.AppendLine(codeString)
-                'outputString.AppendLine()
-            Next i
-
-            Dim outputStreamWriter As New StreamWriter(outputFile)
-            outputStreamWriter.Write(outputString.ToString)
-            outputStreamWriter.Close()
-
-            MsgBox("Data extracted successfully to " & outputFile, vbInformation, "Extraction Complete")
+            Dim xmlDoc As XmlDocument = LoadXmlDocument(inputFile)
+            Dim cheatNodes As XmlNodeList = xmlDoc.SelectNodes("//cheat")
+            Dim extractedContent As String = ExtractCheats(cheatNodes)
+            File.WriteAllText(outputFile, extractedContent)
+            ShowMessage("Data extracted successfully to " & outputFile, "Extraction Complete")
         Catch ex As Exception
-            MsgBox("Error extracting data: " & ex.Message, vbExclamation, "Extraction Error")
+            ShowMessage("Error extracting data: " & ex.Message, "Extraction Error", vbExclamation)
         End Try
 
-        ProgressBar.Value = 100 ' Set progress bar to 0 (optional)
+        ProgressBar.Value = 100
     End Sub
 
+    ' Closes the application
     Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
         Me.Close()
         End
-
     End Sub
 
+    ' Opens the website in the default browser
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-        Process.Start("C:\Program Files\Google\Chrome\Application\chrome.exe", "https://hiroscheatfileconverter.net/")
+        Process.Start("https://hiroscheatfileconverter.net/")
+    End Sub
+
+    ' Shows a file dialog and returns the selected file path
+    Private Function ShowFileDialog(title As String, filter As String, Optional isOpenDialog As Boolean = True) As String
+        Dim dialog As FileDialog
+        If isOpenDialog Then
+            dialog = New OpenFileDialog()
+        Else
+            dialog = New SaveFileDialog()
+        End If
+        dialog.Title = title
+        dialog.Filter = filter
+        If dialog.ShowDialog() = DialogResult.OK Then
+            Return dialog.FileName
+        End If
+        Return String.Empty
+    End Function
+
+    ' Loads an XML document from a file
+    Private Function LoadXmlDocument(filePath As String) As XmlDocument
+        Dim xmlDoc As New XmlDocument()
+        xmlDoc.Load(filePath)
+        Return xmlDoc
+    End Function
+
+    ' Extracts cheat data from XML nodes
+    Private Function ExtractCheats(cheatNodes As XmlNodeList) As String
+        Dim outputString As New StringBuilder()
+        Dim totalCheats As Integer = cheatNodes.Count
+
+        For i As Integer = 0 To totalCheats - 1
+            Dim cheatElement As XmlElement = cheatNodes(i)
+            Dim name As String = cheatElement.GetAttribute("name")
+            Dim codeBlock As String = ExtractCheatCode(cheatElement)
+
+            outputString.AppendLine("!disabled")
+            outputString.AppendLine("#" & name)
+            outputString.AppendLine(codeBlock)
+
+            ProgressBar.Value = ((i + 1) / totalCheats) * 100
+        Next
+
+        Return outputString.ToString()
+    End Function
+
+    ' Extracts code from a single cheat element
+    Private Function ExtractCheatCode(cheatElement As XmlElement) As String
+        Dim codeBlock As New StringBuilder()
+        For Each childNode In cheatElement.ChildNodes
+            If childNode.NodeType = XmlNodeType.Element Then
+                codeBlock.AppendLine(childNode.InnerText)
+            ElseIf childNode.NodeType = XmlNodeType.Text Then
+                Dim textContent As String = childNode.InnerText
+                codeBlock.AppendLine(textContent.Trim(""""c))
+            End If
+        Next
+        Return codeBlock.ToString()
+    End Function
+
+    ' Displays a message box
+    Private Sub ShowMessage(message As String, title As String, Optional style As MsgBoxStyle = vbInformation)
+        MsgBox(message, style, title)
     End Sub
 End Class
---
+++ StartUpForm.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StartUpForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        Label1 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        ToolTip1 = New ToolTip(components)
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 27)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(153, 70)
+        Button1.TabIndex = 0
+        Button1.Text = "mBoy! to mGBA"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(83, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Convert From "
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(12, 103)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(153, 70)
+        Button2.TabIndex = 2
+        Button2.Text = "mGBA to mBoy!"
+        ToolTip1.SetToolTip(Button2, "Unsupported Feature" & vbCrLf & "Will be available" & vbCrLf & "Eventually")
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(9, 179)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(153, 23)
+        Button3.TabIndex = 3
+        Button3.Text = "Quit"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' StartUpForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(174, 215)
+        ControlBox = False
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "StartUpForm"
+        Text = "Start Up Form"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
